a5480def1da58b1c62555080a7962c27ed80a8f3
root <root@localhost>
2024-09-07 20:24:06 +0000

    seeding the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Book.create!([
+    {title: "Harry Potter and the Philosopher's Stone"},
+    {title: "The Lord of the Rings: The Fellowship of the Ring"},
+    {title: "The Catcher in the Rye"},
+    {title: "Diary of a Wimpy Kid"},
+    {title: "Diary of a Wimpy Kid: Rodrick Rules"}
+])
